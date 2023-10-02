f8132456db74fec7e8f83aaabb770895f22803d9
Ritwika28 <ritwika287das2014@gmail.com>
2023-10-02 14:40:27 +0100

    refactoring changes

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ var request = require('request');
 
 request('https://www.gnu.org/licenses/gpl-3.0.txt', function (error: Error, response: any, body: string) {
   if (!error && response.statusCode == 200) {
+        let textObj = {}; 
         const worker = new Worker('./build/worker.js', {
           workerData: {
             value: body,
@@ -21,11 +22,14 @@ request('https://www.gnu.org/licenses/gpl-3.0.txt', function (error: Error, resp
           }
         });
         worker.on('message', (result) => {
-          console.log(result);
-        });
-        app.get('/text', (req,res)=> {
-            res.json({text: body});
+          textObj = result;
+          console.log(textObj);
+          app.get('/text', (req,res)=> {
+            res.json({text: textObj});
         })
+        });
+        
+        
   }
 });
 
